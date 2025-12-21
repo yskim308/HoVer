1e41ea4fc4831786ad88aaed4e244c6e9808f285
Young Seo Kim <youngseokim7@gmail.com>
2025-12-21 17:30:38 +0900

    initialize uv project

--- hover/__init__.py
+++ hover/__init__.py
@@ -1,6 +1,6 @@
 # Student ID number as string.
-STUDENT_ID = ""
+STUDENT_ID = "2021142242"
 
 # Student name as string.
 # This name should match the name that appears on LearnUs.
-STUDENT_NAME = ""
+STUDENT_NAME = "Kim Young Seo"
